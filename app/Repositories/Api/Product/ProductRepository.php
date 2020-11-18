b771878b2089fa77dab1dea1d03369f7a6a20813
Amir Khodabande <amirdp8102@gmail.com>
2020-11-18 17:23:40 +0330

    Index & Show function created, In Repositories

--- app/Repositories/Api/Product/ProductRepository.php
+++ app/Repositories/Api/Product/ProductRepository.php
@@ -2,6 +2,8 @@
 
 namespace App\Repositories\Api\Product;
 
+use App\Http\Resources\Product\ProductResource;
+use App\Product;
 use App\Repositories\Api\Product\ProductRepositoryInterface;
 use Exception;
 use Illuminate\Http\Request;
@@ -9,6 +11,20 @@ use Illuminate\Http\Request;
 
 class ProductRepository implements ProductRepositoryInterface
 {
+    const SUCCUSUS_STATUS_CODE     = 200;
+    const UNAUTHORISED_STATUS_CODE = 401;
+
+    public function index()
+    {
+        $products = Product::paginate(8);
+        return $this->response([$products], self::SUCCUSUS_STATUS_CODE);
+    }
+    public function show(Product $product)
+    {
+        return $this->response([new ProductResource($product)], self::SUCCUSUS_STATUS_CODE);
+        return $this->response([$product], self::SUCCUSUS_STATUS_CODE);
+    }
+
     public function response($data, int $statusCode)
     {
         $response = ["data" => $data, "statusCode" => $statusCode];
--- app/Repositories/Api/Product/ProductRepositoryInterface.php
+++ app/Repositories/Api/Product/ProductRepositoryInterface.php
@@ -2,8 +2,11 @@
 
 namespace App\Repositories\Api\Product;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 interface ProductRepositoryInterface
 {
+    public function index();
+    public function show(Product $product);
 }
