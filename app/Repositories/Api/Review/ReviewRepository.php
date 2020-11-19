c7e64a7b31da1c277c70f281cdf6a8b3888449ec
Amir Khodabande <amirdp8102@gmail.com>
2020-11-19 16:59:09 +0330

    Index method created in ReviewRepository.

--- app/Repositories/Api/Review/ReviewRepository.php
+++ app/Repositories/Api/Review/ReviewRepository.php
@@ -2,13 +2,29 @@
 
 namespace App\Repositories\Api\Review;
 
+use App\Http\Resources\ReviewResource;
+use App\Product;
 use App\Repositories\Api\Review\ReviewRepositoryInterface;
+use App\Review;
 use Exception;
 use Illuminate\Http\Request;
 
 
 class ReviewRepository implements ReviewRepositoryInterface
 {
+    const SUCCUSUS_STATUS_CODE     = 200;
+    const UNAUTHORISED_STATUS_CODE = 401;
+
+    public function index(Product $product)
+    {
+        return ReviewResource::collection($product->reviews()->paginate(5));
+    }
+    public function show(Review $review)
+    {
+        // return new ReviewResource($review);
+    }
+
+
     public function response($data, int $statusCode)
     {
         $response = ["data" => $data, "statusCode" => $statusCode];
--- app/Repositories/Api/Review/ReviewRepositoryInterface.php
+++ app/Repositories/Api/Review/ReviewRepositoryInterface.php
@@ -2,8 +2,12 @@
 
 namespace App\Repositories\Api\Review;
 
+use App\Product;
+use App\Review;
 use Illuminate\Http\Request;
 
 interface ReviewRepositoryInterface
 {
+    public function index(Product $product);
+    public function show(Review $review);
 }
