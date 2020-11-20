ecc6af1f0c337c4186717fa582d807812c683acb
Amir Khodabande <amirdp8102@gmail.com>
2020-11-20 18:38:08 +0330

    user_id Added to the ProductFactory.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,10 +3,14 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Product;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
+        'user_id' => function () {
+            return User::all()->random();
+        },
         'name' => $faker->word,
         'detail' => $faker->paragraph(3),
         'price' => $faker->numberBetween(100, 1000),
