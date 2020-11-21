8f241a7743d740cda546a7799a1cdfb9d4fb666b
Amir Khodabande <amirdp8102@gmail.com>
2020-11-21 17:28:47 +0330

    ReviewRepositories created.

--- app/Repositories/Api/Review/ReviewRepository.php
+++ app/Repositories/Api/Review/ReviewRepository.php
@@ -2,32 +2,73 @@
 
 namespace App\Repositories\Api\Review;
 
+use App\Exceptions\ProductNotBelongsToUser;
+use App\Http\Requests\Review\ReviewRequest;
+use App\Http\Requests\Review\UpdateReviewRequest;
 use App\Http\Resources\ReviewResource;
 use App\Product;
-use App\Repositories\Api\Review\ReviewRepositoryInterface;
 use App\Review;
+use App\Repositories\Api\Review\ReviewRepositoryInterface;
 use Exception;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class ReviewRepository implements ReviewRepositoryInterface
 {
-    const SUCCUSUS_STATUS_CODE     = 200;
-    const UNAUTHORISED_STATUS_CODE = 401;
-
     public function index(Product $product)
     {
         return ReviewResource::collection($product->reviews()->paginate(5));
     }
-    public function show(Review $review)
+    public function show(Product $product, Review $review)
     {
-        // return new ReviewResource($review);
+        if ($review->product_id === $product->id)
+            return new ReviewResource($review);
+        else
+            return response([
+                'errors' => "Sorry This Review is not belong to this product!"
+            ], Response::HTTP_NOT_FOUND);
     }
-
-
-    public function response($data, int $statusCode)
+    public function store(ReviewRequest $request, Product $product)
     {
-        $response = ["data" => $data, "statusCode" => $statusCode];
-        return $response;
+        $review = $product->reviews()->create($request->all());
+        return response([
+            'data' => new ReviewResource($review)
+        ], Response::HTTP_CREATED);
+    }
+    public function update(UpdateReviewRequest $request, Product $product, Review $review)
+    {
+        // We want only the creator of the product can edit/delete Reviews of that product.
+        // 1 : Check if Product belongs to the User.
+        // 2 : Check the review belongs to the selected Product.        
+        $this->ProductUserCheck($product);
+        if ($review->product_id === $product->id) {
+            $review->update($request->all());
+            return response([
+                'data' => new ReviewResource($review)
+            ], Response::HTTP_CREATED);
+        } else
+            return response([
+                'errors' => "Sorry This Review is not belong to this product!"
+            ], Response::HTTP_NOT_FOUND);
+    }
+    public function destroy(Product $product, Review $review)
+    {
+        $this->ProductUserCheck($product);
+        if ($review->product_id === $product->id) {
+            $review->delete();
+            return response([
+                'data' => "Hello"
+            ], Response::HTTP_NO_CONTENT);
+        } else
+            return response([
+                'errors' => "Sorry This Review is not belong to this product!"
+            ], Response::HTTP_NOT_FOUND);
+    }
+    public function ProductUserCheck($product)
+    {
+        if (Auth::id() !== $product->user_id) {
+            throw new ProductNotBelongsToUser();
+        }
     }
 }
--- app/Repositories/Api/Review/ReviewRepositoryInterface.php
+++ app/Repositories/Api/Review/ReviewRepositoryInterface.php
@@ -2,12 +2,16 @@
 
 namespace App\Repositories\Api\Review;
 
+use App\Http\Requests\Review\ReviewRequest;
+use App\Http\Requests\Review\UpdateReviewRequest;
 use App\Product;
 use App\Review;
-use Illuminate\Http\Request;
 
 interface ReviewRepositoryInterface
 {
     public function index(Product $product);
-    public function show(Review $review);
+    public function show(Product $product, Review $review);
+    public function store(ReviewRequest $request, Product $product);
+    public function update(UpdateReviewRequest $request, Product $product, Review $review);
+    public function destroy(Product $product, Review $review);
 }
